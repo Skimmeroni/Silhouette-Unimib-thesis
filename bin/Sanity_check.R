703fcd737ea086c6e2f44976c1e9abec3272b035
shania <federico.salvioni@tutanota.com>
2024-12-04 17:08:45 +0100

    Semplificato il sanity check

--- bin/Sanity_check.R
+++ bin/Sanity_check.R
@@ -1,29 +1,13 @@
-sanity_check_good <- function(package_name) {
+sanity_check <- function(package_name, dataset_path) {
 	file <- paste("bin/Package_", toupper(package_name), ".R", sep = "")
 	source(file)
 
 	# Import the sanity check dataset
-	sc_dataset_good <- read.csv("data/sc_dataset_good.csv")
+	sc_dataset <- read.csv(dataset_path)
 
 	# Apply k-means on the dataset, with 2 centroids
-	sc_clustering_good <- kmeans(sc_dataset_good, centers = 2)
+	sc_clustering <- kmeans(sc_dataset, centers = 2)
 
-	sil_widths_good <- compute_si_Silhouette(sc_clustering_good,
-                                             sc_dataset_good)
-	return(sil_widths_good)
-}
-
-sanity_check_bad <- function(package_name) {
-	file <- paste("bin/Package_", toupper(package_name), ".R", sep = "")
-	source(file)
-
-	# Import the sanity check dataset
-	sc_dataset_bad <- read.csv("data/sc_dataset_bad.csv")
-
-	# Apply k-means on the dataset, with 2 centroids
-	sc_clustering_bad <- kmeans(sc_dataset_bad, centers = 2)
-
-	sil_widths_bad <- compute_si_Silhouette(sc_clustering_bad,
-                                             sc_dataset_bad)
-	return(sil_widths_bad)
+	sil_widths <- compute_si_Silhouette(sc_clustering, sc_dataset)
+	return(sil_widths)
 }
